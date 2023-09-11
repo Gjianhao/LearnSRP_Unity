d2034b357a3607c227cab7b9a0b2a68cca9df88a
gjh <kevinbina@163.com>
2023-09-11 23:32:51 +0800

    创建自定义管线、绘制一个立方体

--- Assets/Settings/HRenderPipelineAsset.cs
+++ Assets/Settings/HRenderPipelineAsset.cs
@@ -11,6 +11,7 @@ namespace Settings {
     }
 
     class HRenderPipeline : RenderPipeline {
+        private ShaderTagId _shaderTag = new ShaderTagId("ForwardBase");
         protected override void Render(ScriptableRenderContext context, Camera[] cameras) {
             foreach (var camera in cameras) {
                 RenderPerCamera(context, camera);
@@ -24,6 +25,19 @@ namespace Settings {
             // 对场景进行裁剪
             camera.TryGetCullingParameters(out var cullingParameters);
             var cullingResults = context.Cull(ref cullingParameters);
+
+            var drawSetting = CreateDrawingSettings(camera);
+            var filterSetting = new FilteringSettings(RenderQueueRange.all);
+            // 绘制物体
+            context.DrawRenderers(cullingResults, ref drawSetting, ref filterSetting);
+        }
+
+        private DrawingSettings CreateDrawingSettings(Camera camera) {
+            // 创建一个排序设置结构体
+            var sortingSetting = new SortingSettings(camera);
+            // 创建一个绘制设置结构体
+            var drawSetting = new DrawingSettings(_shaderTag, sortingSetting);
+            return drawSetting;
         }
     }
 }

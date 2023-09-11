83491e86b64eba42ca7e792e26ba8d6a5023f8fb
gjh <kevinbina@163.com>
2023-09-11 22:07:56 +0800

    设置相机参数、裁剪剔除

--- Assets/Settings/HRenderPipelineAsset.cs
+++ Assets/Settings/HRenderPipelineAsset.cs
@@ -19,7 +19,11 @@ namespace Settings {
         }
 
         private void RenderPerCamera(ScriptableRenderContext context, Camera camera) {
-            
+            // 将camera相关参数，设置到渲染管线中
+            context.SetupCameraProperties(camera);
+            // 对场景进行裁剪
+            camera.TryGetCullingParameters(out var cullingParameters);
+            var cullingResults = context.Cull(ref cullingParameters);
         }
     }
 }

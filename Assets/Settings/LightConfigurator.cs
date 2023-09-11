c8577c886b26a68c39b5a1315bf93888a6a8e412
gjh <kevinbina@163.com>
2023-09-11 23:53:13 +0800

    【2】创建光源配置类

---
+++ Assets/Settings/LightConfigurator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Settings {
+    public class LightConfigurator {
+        private static int CompareLightRenderMode(LightRenderMode mode1, LightRenderMode mode2) {
+            if (mode1 == mode2) {
+                return 0;
+            }
+
+            if (mode1 == LightRenderMode.ForcePixel) {
+                return -1;
+            }
+
+            if (mode2 == LightRenderMode.ForcePixel) {
+                return 1;
+            }
+
+            if (mode1 == LightRenderMode.Auto) {
+                return -1;
+            }
+
+            if (mode2 == LightRenderMode.Auto) {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
